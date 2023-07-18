25952b5b02c516def7626a9bc681f69ca29d09c5
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-07-18 10:09:19 -0300

    Test React
    A presentation with React

---
+++ proj1/src/App.jsx
@@ -0,0 +1,11 @@
+import Tittle from "./Tittle"
+
+function App(){
+ return ( 
+ <div>
+    <Tittle />
+  </div>
+  )
+}
+
+export default App
---
+++ proj1/src/Tittle.jsx
@@ -0,0 +1,22 @@
+function Tittle() {
+
+    let name = "Murilo"
+    const age = 22
+    const url = "https://www.freecodecamp.org/portuguese/news/content/images/2023/03/Ekran-Resmi-2019-11-18-18.08.13.png"
+    return (
+        <div>
+         <h1>Hi i'm learning React JS. My name is {name}, i'm {age} years old and i'm a Developer</h1>
+         <img width={600} src={url} />
+        </div>
+         )
+}
+
+export default Tittle
+
+/* Notes
+
+State:
+
+
+*/
+
